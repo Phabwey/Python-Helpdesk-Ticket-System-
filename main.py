a0ec6205fbdbcbb61a0f74e4a85c6a7f6a4453ff
Phabwey <phabweylitabe98@gmail.com>
2026-07-10 23:26:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,23 @@ while True:
     choice = input("Choose an option: ")
 
     if choice == "1":
+
         try:
-    ticket_id = int(input("Ticket ID: "))
-except ValueError:
-    print("Please enter a valid ticket number.")
-    continue
+            ticket_id = int(input("Ticket ID: "))
+        except ValueError:
+            print("Please enter a valid ticket number.")
+            continue
+
         customer_name = input("Customer name: ")
         issue = input("Issue: ")
-        priority = input("Priority (Low, Medium, High): ")
+
+        priority = input(
+            "Priority (Low, Medium, High): "
+        ).capitalize()
+
+        if priority not in ["Low", "Medium", "High"]:
+            print("Invalid priority.")
+            continue
 
         manager.create_ticket(
             ticket_id,
@@ -34,8 +43,16 @@ except ValueError:
         manager.search_ticket(ticket_id)
 
     elif choice == "4":
+
         ticket_id = input("Ticket ID: ")
-        status = input("New status: ")
+
+        status = input(
+            "New status (Open, In Progress, Closed): "
+        ).title()
+
+        if status not in ["Open", "In Progress", "Closed"]:
+            print("Invalid status.")
+            continue
 
         manager.update_status(ticket_id, status)
 
